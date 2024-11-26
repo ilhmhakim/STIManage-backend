678d5af8ad4b8e03ba37f2f6169458f4d865ab4b
Ilham Hakim <ilhmhakim@gmail.com>
2024-11-26 15:27:08 +0700

    add more routes

--- app/src/route/private-api.ts
+++ app/src/route/private-api.ts
@@ -1,9 +1,22 @@
 import express from "express";
 import {authMiddleware} from "../middleware/auth-middleware";
 import {TaskController} from "../controller/task-controller";
+import {UserController} from "../controller/user-controller";
 
 export const privateRouter = express.Router();
 // @ts-ignore
-privateRouter.post("/api/v1/task", authMiddleware, TaskController.createTask)
+privateRouter.post("/api/v1/task", authMiddleware, TaskController.createTask);
 // @ts-ignore
-privateRouter.patch("/api/v1/task/:taskId(\\d+)", authMiddleware, TaskController.createTask)
+privateRouter.patch("/api/v1/task/:taskId(\\d+)", authMiddleware, TaskController.updateTask);
+// @ts-ignore
+privateRouter.get("/api/v1/task/:taskId(\\d+)", authMiddleware, TaskController.getTaskDetail);
+// @ts-ignore
+privateRouter.delete("/api/v1/task/:taskId(\\d+)", authMiddleware, TaskController.deleteTask);
+// @ts-ignore
+privateRouter.get("/api/v1/tasks/summary", authMiddleware, TaskController.getSummary);
+// @ts-ignore
+privateRouter.get("/api/v1/tasks", authMiddleware, TaskController.getDashboard)
+// @ts-ignore
+privateRouter.get("/api/v1/tasks/download", authMiddleware, TaskController.downloadDashboard)
+// @ts-ignore
+privateRouter.delete("/api/v1/logout", authMiddleware, UserController.logout)
--- app/src/route/public-api.ts
+++ app/src/route/public-api.ts
@@ -4,3 +4,4 @@ import {UserController} from "../controller/user-controller";
 export const publicRouter = express.Router();
 publicRouter.post("/api/v1/registration", UserController.register);
 publicRouter.post("/api/v1/login", UserController.login);
+publicRouter.post("/api/v1/refresh", UserController.refreshAccessToken);
