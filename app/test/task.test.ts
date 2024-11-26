f16ed4cf99e3eb3703928fdb1587e9d0f10ce9b2
Ilham Hakim <ilhmhakim@gmail.com>
2024-11-26 15:24:27 +0700

    completed user and task tests

--- app/test/task.test.ts
+++ app/test/task.test.ts
@@ -10,9 +10,9 @@ describe('POST /api/v1/task', () => {
     it('Menerima request pembuatan task baru saat semua datanya valid', async () => {
         const response = await supertest(web)
             .post("/api/v1/task")
-            .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QxMjM0IiwiaWF0IjoxNzMyNTM2MTkxLCJleHAiOjE3MzI1NDMzOTF9.neH4l7Ov5om4f-Ti-0y-_KsnDRznVcsXCaCear9gAVI")
+            .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QxMjM0IiwiaWF0IjoxNzMyNjA4NjI5LCJleHAiOjE3MzI2MTU4Mjl9.tgIMkktY3pl-1XAhB4uiCSvmntzBvpIlfNnAD9bc6JU")
             .send({
-                task_name: "Test 1",
+                task_name: "Test ACC BARU COY",
                 gitlab_link: "url.com",
                 scope: "BE",
                 module_type: "DTS User",
@@ -25,5 +25,73 @@ describe('POST /api/v1/task', () => {
 
         logger.debug(response.body);
         expect(response.status).toBe(201);
+    });
+});
+
+describe('PATCH /api/v1/task', () => {
+    it('Menerima request pembuatan task baru saat semua datanya valid', async () => {
+        const response = await supertest(web)
+            .patch("/api/v1/task/4")
+            .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QxMjM0IiwiaWF0IjoxNzMyNTkxMzU4LCJleHAiOjE3MzI1OTg1NTh9.57Gn13FpgDcxC-DozUM5Kfbc3nOFY47zg3FqQOpEPH8")
+            .send({
+                task_name: "Test 3",
+                gitlab_link: "url.com",
+                scope: "BE",
+                module_type: "DTS User",
+                problem_type: "Problem Test",
+                category: "Fitur Baru",
+                programmer_name: "Ethan",
+                qa_name: "Ethan QA",
+                deadline_date: "2024-11-29",
+                task_status: "Doing"
+            });
+
+        logger.debug(response.body);
+        expect(response.status).toBe(201);
+    });
+});
+
+describe('GET /api/v1/task', () => {
+    it('Menerima request untuk get task detail', async () => {
+        const response = await supertest(web)
+            .get("/api/v1/task/4")
+            .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QxMjM0IiwiaWF0IjoxNzMyNTkxMzU4LCJleHAiOjE3MzI1OTg1NTh9.57Gn13FpgDcxC-DozUM5Kfbc3nOFY47zg3FqQOpEPH8")
+
+        logger.debug(response.body);
+        expect(response.status).toBe(200);
+        expect(response.body.data).toBeDefined();
+    });
+});
+
+describe('DELETE /api/v1/task', () => {
+    it('Menerima request untuk menghapus suatu task', async () => {
+        const response = await supertest(web)
+            .delete("/api/v1/task/4")
+            .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QxMjM0IiwiaWF0IjoxNzMyNTkxMzU4LCJleHAiOjE3MzI1OTg1NTh9.57Gn13FpgDcxC-DozUM5Kfbc3nOFY47zg3FqQOpEPH8")
+
+        logger.debug(response.body);
+        expect(response.status).toBe(200);
+    });
+});
+
+describe('GET /api/v1/tasks/summary', () => {
+    it('Mengakses summary dari keseluruhan status tasks', async () => {
+        const response = await supertest(web)
+            .get("/api/v1/tasks")
+            .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QxMjM0IiwiaWF0IjoxNzMyNTk5NjMyLCJleHAiOjE3MzI2MDY4MzJ9.Wy6_l1nWKzyuoF3HKCK1MyINYM8hG5l2jlAKSlqNqMg")
+
+        logger.debug(response.body);
+        expect(response.status).toBe(200);
+    });
+});
+
+describe('GET /api/v1/tasks/download', () => {
+    it('Download seluruh tasks', async () => {
+        const response = await supertest(web)
+            .get("/api/v1/tasks/download")
+            .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QxMjM0IiwiaWF0IjoxNzMyNTk5NjMyLCJleHAiOjE3MzI2MDY4MzJ9.Wy6_l1nWKzyuoF3HKCK1MyINYM8hG5l2jlAKSlqNqMg")
+
+        logger.debug(response.body);
+        expect(response.status).toBe(200);
     });
 });
--- app/test/user.test.ts
+++ app/test/user.test.ts
@@ -32,3 +32,28 @@ describe('POST /api/v1/login', () => {
         expect(response.body.data.refresh_token).toBeDefined();
     });
 });
+
+describe('POST /api/v1/refresh', () => {
+    it('Menerima access token yang baru', async () => {
+        const response = await supertest(web)
+            .post("/api/v1/refresh")
+            .send({
+                refresh_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QxMjM0IiwiaWF0IjoxNzMyNjA4NTQ5LCJleHAiOjE3MzMyMTMzNDl9.iSRgYbIeNYaiWahnvZI0vZSfWE0w_KFF2vXgIkmoJQc"
+            });
+
+        logger.debug(response.body);
+        expect(response.status).toBe(200);
+        expect(response.body.data.access_token).toBeDefined();
+    });
+});
+
+describe('DELETE /api/v1/logout', () => {
+    it('Download seluruh tasks', async () => {
+        const response = await supertest(web)
+            .delete("/api/v1/logout")
+            .set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QxMjM0IiwiaWF0IjoxNzMyNjA4NjI5LCJleHAiOjE3MzI2MTU4Mjl9.tgIMkktY3pl-1XAhB4uiCSvmntzBvpIlfNnAD9bc6JU")
+
+        logger.debug(response.body);
+        expect(response.status).toBe(200);
+    });
+});
